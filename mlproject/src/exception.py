4cbf0fedd64286e907dd98cafb293922aab0babc
Manoj-Kumar-03 <lingala.manoj3@gmail.com>
2026-06-17 23:21:35 +0530

    Data Transformation Completed

--- mlproject/src/exception.py
+++ mlproject/src/exception.py
@@ -4,7 +4,7 @@ from src.logger_1 import logging
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb =error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error Occured in python script [ {0} ] line number [v{1} ] and error is [ {2} ]".format(
+    error_message = "Error Occured in python script [ {0} ] line number [ {1} ] and error is [ {2} ]".format(
         file_name,exc_tb.tb_lineno,str(error)
     )
 
